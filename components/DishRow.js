33edd9463abd5c92cbb2eba64ade6bf6a7b177ac
Yongjae Kim <mearak43@gmail.com>
2025-02-20 20:34:49 +0900

    feat: redux, basket functionality and basket pop up

--- components/DishRow.js
+++ components/DishRow.js
@@ -5,14 +5,31 @@ import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
 
 import sanityClient, { urlFor } from "../sanity";
 import { REACT_NATIVE_COLOR } from "../constants/colors";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItems,
+  selectBasketItemsWithId,
+} from "../features/basketSlice";
 
 const DishRow = ({ id, name, description, price, image }) => {
   const [isPressed, setIsPressed] = useState(false);
+
+  const dispatch = useDispatch();
+  const items = useSelector((state) => selectBasketItemsWithId(state, id));
+  const addItem = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+  const removeItem = () => {
+    dispatch(removeFromBasket({ id }));
+  };
+
   return (
     <>
       <TouchableOpacity
         onPress={() => setIsPressed(!isPressed)}
-        className={`p-4 pb-2 bg-white mb-2 border-gray-200 ${
+        className={`bg-white border p-4 border-gray-200 ${
           isPressed && "border-b-0"
         }`}
         key={id}
@@ -36,22 +53,25 @@ const DishRow = ({ id, name, description, price, image }) => {
             />
           </View>
         </View>
-        {isPressed && (
-          <View className="bg-white mt-4">
-            <View className="flex-row items-center gap-x-2">
-              <TouchableOpacity>
-                <MinusCircleIcon color={REACT_NATIVE_COLOR} size={40} />
-              </TouchableOpacity>
-
-              <Text>0</Text>
-
-              <TouchableOpacity>
-                <PlusCircleIcon color={REACT_NATIVE_COLOR} size={40} />
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
       </TouchableOpacity>
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center gap-x-2 pb-3">
+            <TouchableOpacity disabled={!items.length} onPress={removeItem}>
+              <MinusCircleIcon
+                color={items.length > 0 ? REACT_NATIVE_COLOR : "grey"}
+                size={40}
+              />
+            </TouchableOpacity>
+
+            <Text>{items.length}</Text>
+
+            <TouchableOpacity onPress={addItem}>
+              <PlusCircleIcon color={REACT_NATIVE_COLOR} size={40} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </>
   );
 };
